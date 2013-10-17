be34b1a7a9dd6de6f009b4edb197259f3314b449
Igor Khotin <khotin@gmx.com>
2013-10-18 00:27:44 +0300

    [+] created empty maven project

---
+++ src/main/java/com/decaflabs/trux/App.java
@@ -0,0 +1,13 @@
+package com.decaflabs.trux;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
