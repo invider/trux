eccae06ecd28223b7974bfe744dd594e225118b5
Ihor Dyuba <idyuba@gmail.com>
2013-11-01 11:38:59 +0200

    [+] added Red trux

---
+++ src/main/java/com/decaflabs/trux/platform/trux/RedTrux.java
@@ -0,0 +1,43 @@
+package com.decaflabs.trux.platform.trux;
+
+import com.decaflabs.trux.platform.AbstractTrux;
+
+public class RedTrux extends AbstractTrux {
+
+	private static final int TEAM = 1;
+	
+	private static final double SPEED = 10d;
+	
+	private int NAPRAVLENIE = 0; // 0 - Stop, -1 west, +1 east
+	
+	private void setNapravlenie(int value) {
+		switch(value){
+		case -1:
+			NAPRAVLENIE = -1;
+			break;
+		case 0:
+			NAPRAVLENIE = 0;
+			break;
+		case 1:
+			NAPRAVLENIE = 1;
+			break;
+		default:
+			NAPRAVLENIE = 1;
+			break;
+		}
+	}
+	
+	public RedTrux(double x, int napravlenie) {
+		super(TEAM);
+		super.setX(x);
+		super.setY(0);
+		
+		setNapravlenie(napravlenie);
+	}
+	
+	public void mutate(double delta) {
+		this.setX(this.getX() + NAPRAVLENIE * this.SPEED * delta);
+	}
+
+
+}
